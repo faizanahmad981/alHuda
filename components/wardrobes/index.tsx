215dc4769c6c7c2ab562686d5ae14cfd1172c6f5
khawajfaisal010 <khawajfaisal010@email.com>
2025-04-15 15:25:58 +0500

    fixessss

--- components/wardrobes/index.tsx
+++ components/wardrobes/index.tsx
@@ -18,7 +18,8 @@ const Wardrobes = () => {
             </h1>
             <div className="bg-lowpoly text-2xl flex py-12">
                 <p className='mx-auto'>Our custom carpentry expertise brings you exquisite HomeFurniture
-                     furniture that elevates <br /> your workspace. From sleek desks to sophisticated storage solutions, we craft  <br /> pieces that combine style, comfort, and durability.
+                     furniture that elevates <br /> your workspace. From sleek desks to sophisticated storage solutions, we craft
+                       <br /> pieces that combine style, comfort, and durability.
                 </p>
             </div>
         </div>
